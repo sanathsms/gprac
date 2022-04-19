10ef53f382e69eedd93df144b725fcfd6ba8a0e6
sanath.basavapathri@gmail.com <sanath.basavapathri@gmail.com>
2022-04-20 00:50:02 +0530

    san

--- TA-1SimpleCrudOpertions - Copy/src/main/java/com/example/demo/ControllerAndDAO/CrudController.java
+++ TA-1SimpleCrudOpertions - Copy/src/main/java/com/example/demo/ControllerAndDAO/CrudController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Model.Ticket;
-
+// comment added
 @RestController
 @RequestMapping(value = "/")
 public class CrudController {
